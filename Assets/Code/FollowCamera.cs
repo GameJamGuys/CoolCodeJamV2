df0db54d80d820e6d8d256b4769b5d9754e40872
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-04-06 16:45:04 +0300

    Char changes

--- Assets/Code/FollowCamera.cs
+++ Assets/Code/FollowCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] float interpolationFactor = 0.0f;
+    [SerializeField] float interpolationFactor = 1.0f;
     [SerializeField] float zDistance = 10.0f;
     public GameObject target;
 
